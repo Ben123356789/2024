1a16f81ab3fcb96c4aa663332fe5b8ad46c24081
rambotics-new-email <156865794+rambotics-new-email@users.noreply.github.com>
2024-01-25 17:47:47 -0500

    Yippie!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,14 +3,14 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import frc.robot.subsystems.PigeonSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class RobotContainer {
   public Subsystem pigeonSubsystem;
 
   public RobotContainer() {
     configureBindings();
-    pigeonSubsystem = new PigeonSubsystem();
+    pigeonSubsystem = new ShooterSubsystem();
   }
 
   private void configureBindings() {}
