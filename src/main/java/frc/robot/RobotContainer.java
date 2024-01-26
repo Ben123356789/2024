3910f9fe2c0f9397ff51930c08a0e8862f0cbfa4
rambotics-new-email <156865794+rambotics-new-email@users.noreply.github.com>
2024-01-25 20:40:33 -0500

    led stuff idk

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,16 +1,26 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.PigeonSubsystem;
 
 public class RobotContainer {
-  public Subsystem pigeonSubsystem;
+  //public Subsystem pigeonSubsystem;
+  public LEDSubsystem ledSubsystem;
+  public LimelightSubsystem limelight1;
+  public PowerDistribution pdp;
 
   public RobotContainer() {
     configureBindings();
-    pigeonSubsystem = new ShooterSubsystem();
+    //pigeonSubsystem = new PigeonSubsystem();
+    limelight1 = new LimelightSubsystem();
+    pdp = new PowerDistribution(1, ModuleType.kRev);
+    ledSubsystem = new LEDSubsystem(limelight1, pdp);
   }
 
   private void configureBindings() {}
--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -2,8 +2,10 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
 
 public class ExampleSubsystem extends SubsystemBase {
+
   public ExampleSubsystem() {
 
   }
