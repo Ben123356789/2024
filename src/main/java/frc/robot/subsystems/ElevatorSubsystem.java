dd5f051d93abe032a8b4b8cda3381c9a891f3ac5
Cerulity64X <cerulity32k@gmail.com>
2024-01-27 09:54:47 -0500

    Implement ElevatorSubsystem revision 1, make FlumperState public.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,14 +1,27 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import com.revrobotics.CANSparkBase.ControlType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.JMath;
+import frc.robot.PIDMotor;
 
 public class ElevatorSubsystem extends SubsystemBase {
-  public ElevatorSubsystem() {
-    penis
-  }
+    PIDMotor elevatorMotor = new PIDMotor(Constants.ELEVATOR_ID, 0, 0, 0, 0, ControlType.kPosition, 0);
 
-  @Override
-  public void periodic() {
-  }
+    public ElevatorSubsystem() {}
+
+    double target = Constants.ELEVATOR_HEIGHT / 2;
+
+    public void set(double height) {
+        setUnchecked(JMath.clamp(height, 0.0, Constants.ELEVATOR_HEIGHT));
+    }
+
+    void setUnchecked(double height) {
+        double pos = JMath.map(height, 0.0, Constants.ELEVATOR_HEIGHT, Constants.ELEVATOR_LOWEST, Constants.ELEVATOR_HIGHEST);
+        elevatorMotor.target(pos);
+    }
+
+    @Override public void periodic() { }
 }
--- src/main/java/frc/robot/subsystems/FlumperSubsystem.java
+++ src/main/java/frc/robot/subsystems/FlumperSubsystem.java
@@ -3,12 +3,11 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class FlumperSubsystem extends SubsystemBase {
-  enum FlumperState {
+  public enum FlumperState {
     Eat,
     Stop,
     Spit;
