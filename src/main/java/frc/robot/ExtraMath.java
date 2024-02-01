9360215a73fb99576bc5ae43526f85b0a06fba22
Logan <loganlagercrantz.2006@gmail.com>
2024-02-01 12:55:46 -0500

    test

--- src/main/java/frc/robot/ExtraMath.java
+++ src/main/java/frc/robot/ExtraMath.java
@@ -4,6 +4,7 @@ public final class ExtraMath {
     private ExtraMath() {}
 
     // Forces a value into a set range
+    // For more info: https://www.desmos.com/calculator/kfyws5ap6u
     public static double clamp(double val, double start, double end) {
         double min = Math.min(start, end);
         double max = Math.max(start, end);
@@ -28,6 +29,8 @@ public final class ExtraMath {
     }
 
     // Maps a value from one range to another
+    // For more info: https://stackoverflow.com/questions/5731863/mapping-a-numeric-range-onto-another |
+    // https://www.desmos.com/calculator/0zisstnbzz
     public static double rangeMap(double val, double low1, double high1, double low2, double high2) {
         return ((high2 - low2) / (high1 - low1)) * (val - low1) + low2;
     }
