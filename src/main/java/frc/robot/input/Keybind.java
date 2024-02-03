159543f6c27e7824cfd2af3e43aa0159933e0d14
Cerulity64X <cerulity32k@gmail.com>
2024-02-03 13:06:58 -0500

    Add Keybind class.

---
+++ src/main/java/frc/robot/input/Keybind.java
@@ -0,0 +1,42 @@
+package frc.robot.input;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class Keybind implements BooleanSupplier {
+    public enum Button {
+        A, B, X, Y,
+        LeftBumper, RightBumper,
+        LeftStick, RightStick;
+
+        public Supplier<Boolean> getMethod(XboxController controller) {
+            switch (this) {
+                case A: return controller::getAButton;
+                case B: return controller::getBButton;
+                case X: return controller::getXButton;
+                case Y: return controller::getYButton;
+                case LeftBumper: return controller::getLeftBumper;
+                case RightBumper: return controller::getRightBumper;
+                case LeftStick: return controller::getLeftStickButton;
+                case RightStick: return controller::getRightStickButton;
+                default: return () -> false;
+            }
+        }
+    }
+    XboxController hid;
+    public Button button;
+    public Keybind(XboxController controller, Button button) {
+        hid = controller;
+        this.button = button;
+    }
+    @Override public boolean getAsBoolean() {
+        return button.getMethod(hid).get();
+    }
+
+    public Trigger trigger() {
+        return new Trigger(this);
+    }
+}
